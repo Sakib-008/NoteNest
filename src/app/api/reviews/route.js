af502b7962920e8e38961b0f95e8a0c375ade819
Asif Istiaque <asifistiaq47@gmail.com>
2026-07-10 01:55:08 +0600

    SCRUM-44 Update Notes with Average Rating

--- src/app/api/reviews/route.js
+++ src/app/api/reviews/route.js
@@ -17,6 +17,25 @@ export async function POST(request) {
       },
     });
 
+    const reviews = await prisma.review.findMany({
+      where: {
+        noteId,
+      },
+    });
+
+    const averageRating =
+      reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length;
+
+    await prisma.note.update({
+      where: {
+        id: noteId,
+      },
+
+      data: {
+        averageRating,
+      },
+    });
+
     return NextResponse.json({
       message: "Review Submitted",
     });
